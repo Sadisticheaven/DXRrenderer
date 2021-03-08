c0c7225d8bf1396d338a3cc9fb412306ac272bad
区恒煜 <347563303@qq.com>
2021-03-08 12:14:54 +0800

    为每个HitGroup绑定了对应的材质属性并添加了材质属性的默认构造函数

--- HLSLCompat.h
+++ HLSLCompat.h
@@ -35,7 +35,7 @@ typedef UINT32 Index;
 struct PayLoad
 {
 	XMFLOAT4 colorAndDistance;
-    UINT recursionDepth;
+	UINT recursionDepth;
 };
 
 struct Vertex
@@ -50,31 +50,37 @@ struct SceneConstants
 	XMMATRIX projection;
 	XMMATRIX viewI;
 	XMMATRIX projectionI;
-    UINT spp;
+	UINT spp;
 };
 
 namespace MaterialType {
-    enum Type {
-        Default,
-        Matte,      // Lambertian scattering
-        Mirror,     // Specular reflector that isn't modified by the Fresnel equations.
-        AnalyticalCheckerboardTexture
-    };
+	enum Type {
+		Default = 0,
+		Matte,      // Lambertian scattering
+		Mirror,     // Specular reflector that isn't modified by the Fresnel equations.
+		AnalyticalCheckerboardTexture,
+		Count
+	};
 }
 struct PrimitiveMaterialBuffer
 {
-    XMFLOAT3 Kd;
-    XMFLOAT3 Ks;
-    XMFLOAT3 Kr;
-    XMFLOAT3 Kt;
-    XMFLOAT3 opacity;
-    XMFLOAT3 emit;
-    float roughness;
-    //BOOL hasDiffuseTexture;
-    //BOOL hasNormalTexture;
-    //BOOL hasPerVertexTangents;
-    MaterialType::Type type;
-    float padding;
+	XMFLOAT3 Kd;
+	XMFLOAT3 Ks;
+	XMFLOAT3 Kr;
+	XMFLOAT3 Kt;
+	XMFLOAT3 opacity;
+	XMFLOAT3 emit;
+	float roughness;
+	//BOOL hasDiffuseTexture;
+	//BOOL hasNormalTexture;
+	//BOOL hasPerVertexTangents;
+	MaterialType::Type type;
+	float padding;
+#ifndef HLSL
+	PrimitiveMaterialBuffer() :Kd(0, 0, 0), Ks(0, 0, 0), Kr(0, 0, 0), Kt(0, 0, 0), opacity(0, 0, 0), emit(0, 0, 0), roughness(0), type(MaterialType::Matte), padding(0) {
+
+	}
+#endif
 };
 // Attributes output by the raytracing when hitting a surface,
 // here the barycentric coordinates
